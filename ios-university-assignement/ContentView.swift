73dd5c12a1dcf48d7a20371071fd120f543b0337
K.K.Wenuja <75471327+INTEL-ONBOARD@users.noreply.github.com>
2026-06-27 09:49:40 +0530

    Initial Commit

---
+++ ios-university-assignement/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ios-university-assignement
+//
+//  Created by KKwenuja on 2026-06-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios-university-assignement/ios_university_assignementApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_university_assignementApp.swift
+//  ios-university-assignement
+//
+//  Created by KKwenuja on 2026-06-27.
+//
+
+import SwiftUI
+
+@main
+struct ios_university_assignementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
